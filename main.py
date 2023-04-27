a53be6864d238c37c4227a65d74521ac1170d83a
jxcob <starus3@gmail.com>
2023-04-27 21:01:36 +0200

    player

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
 import pygame
 
 WIDTH , HEIGHT = 1280, 720
-FPS= 60
+FPS = 60
 WIN = pygame.display.set_mode((WIDTH,HEIGHT))
 BG = pygame.image.load("textures/mapa.jpeg")
+PLAYER_WIDTH = 40
+PLAYER_HEIGHT = 40
+
 pygame.display.set_caption("Dupa")
 
-def draw():
+
+def draw(player):
     WIN.blit(BG, (0, 0))
+    pygame.draw.rect(WIN, "blue", player)
     pygame.display.update()
 
 def main():
     run = True
+    player = pygame.Rect(WIDTH/2, HEIGHT/2, PLAYER_WIDTH, PLAYER_HEIGHT)
 
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
-        draw()
+        draw(player)
     pygame.quit()
 
 if __name__ == "__main__":
